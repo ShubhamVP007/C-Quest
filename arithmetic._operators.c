6b9328964f3327a833cbf1ec908dc26563c2e795
Shubham Patil <shubhamvpatil2308@gmail.com>
2026-09-16 21:36:26 +0530

    Update arithmetic._operators.c

--- arithmetic._operators.c
+++ arithmetic._operators.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
 int main() {
+    
     int num1, num2;
 
     printf("enter two numbers : ");
+    
     scanf("%d %d", &num1, &num2);
 
     printf("sum = %d\n", num1 + num2);
